cc20c08dcb6515ec0893614bfc4f7f67ea1bb08f
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-19 11:40:23 +0200

    Add files via upload

---
+++ StringOperations/StringSubstring.java
@@ -0,0 +1,8 @@
+public class StringSubstring {
+
+    public static void main(String[] args) {
+        String s = "This is some text";
+        String substring = s.substring(0,8);
+        System.out.println(substring);
+    }
+}
