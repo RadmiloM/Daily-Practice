7b422568ed1cb1f9f73c29095975dd743cddf8d4
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-20 10:51:54 +0200

    Add files via upload

---
+++ StringOperations/StringConcatenation.java
@@ -0,0 +1,11 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String firstString = "Today is great ";
+        String secondString = firstString + "day";
+        System.out.println(secondString);
+        String strFirst = "This is example with ";
+        String strSecond = strFirst.concat("concatenation");
+        System.out.println(strSecond);
+    }
+
+}
