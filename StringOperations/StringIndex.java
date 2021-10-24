ff94a131ed52aa7cdb6e3bcd0324a3582705adb3
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-24 22:10:52 +0200

    Add files via upload

---
+++ StringOperations/StringIndex.java
@@ -0,0 +1,7 @@
+public class StringIndex {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        String p = "Hello Moon";
+        System.out.println(p.indexOf("Moon"));
+    }
+}
