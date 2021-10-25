4651afc7b6e053c4d3fa7717ff926b91431fac71
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-25 11:41:04 +0200

    Add files via upload

---
+++ StringOperations/StringLowerCase.java
@@ -0,0 +1,6 @@
+public class StringLowerCase {
+    public static void main(String[] args) {
+        String s = "THIS IS STRING CONVERSION FROM UPPERCASE TO LOWERCASE";
+        System.out.println(s.toLowerCase());
+    }
+}
