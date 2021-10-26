5763513e5b98a540655e0b514223ce7e8d8feaa6
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-26 19:53:37 +0200

    Add files via upload

---
+++ StringOperations/StringUppercase.java
@@ -0,0 +1,6 @@
+public class StringUppercase {
+    public static void main(String[] args) {
+        String s = "this is the text in lowercase which will be converted in uppercase";
+        System.out.println(s.toUpperCase());
+    }
+}
