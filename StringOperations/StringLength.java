f1b429cb7721f95fc971c42b942867974520b6e0
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-28 18:38:27 +0200

    Add files via upload

---
+++ StringOperations/StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "This is the string for checking length";
+        System.out.println(str.length());
+    }
+}
