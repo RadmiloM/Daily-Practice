64b795e4917c282537414b5f7e92e13eb3c6c305
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-29 10:55:51 +0200

    Add files via upload

---
+++ StringOperations/StringEquals.java
@@ -0,0 +1,9 @@
+public class StringEquals {
+    public static void main(String[] args) {
+        String s = "Computer";
+        String s1 = "Mouse";
+        String s2 = "Computer";
+        System.out.println(s.equals(s1));
+        System.out.println(s.equals(s2));
+    }
+}
