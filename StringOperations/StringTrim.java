26585119e3d9926f1f9dadf085f874c06a23779d
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-10-30 20:39:41 +0200

    Add files via upload

---
+++ StringOperations/StringTrim.java
@@ -0,0 +1,7 @@
+public class StringTrim {
+    public static void main(String[] args) {
+        String s = " Some text with wide spaces ";
+        System.out.println(s);
+        System.out.println(s.trim());
+    }
+}
