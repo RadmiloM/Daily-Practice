392f23f7f599ebc8736aa7b8b6292309bb4ba5d7
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-01 16:48:45 +0100

    Add files via upload

---
+++ StringCompareTo.java
@@ -0,0 +1,7 @@
+public class StringCompareTo {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        String s1 = "Helllo World";
+        System.out.println(s.compareTo(s1));
+    }
+}
