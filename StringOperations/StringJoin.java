5163bc50c6a8b14c0b701e46a57eb4b571f57248
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-08 22:26:44 +0100

    Add files via upload

---
+++ StringOperations/StringJoin.java
@@ -0,0 +1,6 @@
+public class StringJoin {
+    public static void main(String[] args) {
+        String[] words = new String[]{"Football","Basketball","Tenis"};
+        System.out.println(String.join("-",words));
+    }
+}
