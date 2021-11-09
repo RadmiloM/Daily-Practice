a2b530990fc896020a1aae7cc4cbe14054baf907
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-09 11:29:31 +0100

    Add files via upload

---
+++ StringOperations/StringFormat.java
@@ -0,0 +1,8 @@
+public class StringFormat {
+    public static void main(String[] args) {
+        String s = "Box";
+        String s1 = "Second box";
+        String s2 = "Third box";
+        System.out.printf("%s is chocolate,%s is strawberry,%s is apple" ,s,s1,s2);
+    }
+}
