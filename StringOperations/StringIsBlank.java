135d16035a5eca30e1610e0ac050f038b8f97972
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-10 22:21:05 +0100

    Add files via upload

---
+++ StringOperations/StringIsBlank.java
@@ -0,0 +1,8 @@
+public class StringIsBlank {
+    public static void main(String[] args) {
+        String s = "This is some text";
+        System.out.println(s.isBlank());
+        String s2 = "";
+        System.out.println(s2.isBlank());
+    }
+}
