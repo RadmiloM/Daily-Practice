0d1b74cb49e3dfe250bf58fb5b1707c0589478cd
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-12 08:51:01 +0100

    Add files via upload

---
+++ StringOperations/StringReplaceFirst.java
@@ -0,0 +1,6 @@
+public class StringReplaceFirst {
+    public static void main(String[] args) {
+        String s = "this is some story";
+        System.out.println(s.replaceFirst("t","o"));
+    }
+}
