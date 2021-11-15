ac2c4b43fe7262d4695a25c8b4039d45c5e16adf
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-15 11:21:00 +0100

    Add files via upload

---
+++ StringOperations/StringCodePointBefore.java
@@ -0,0 +1,6 @@
+public class StringCodePointBefore {
+    public static void main(String[] args) {
+        String s = "People should read books every day";
+        System.out.println(s.codePointBefore(1));
+    }
+}
