b714de11f4020828089ffcbbd49d9e4e0d7df5cb
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-17 12:28:55 +0100

    Add files via upload

---
+++ StringOperations/StringChars.java
@@ -0,0 +1,8 @@
+public class StringChars {
+    public static void main(String[] args) {
+        String s = "Hello World today";
+        for(int i =0 ; i < s.length();i++){
+            System.out.println(s.chars());
+        }
+    }
+}
