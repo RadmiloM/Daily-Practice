98c7f967a7de69c670bc6788c7e8412028138bbf
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-18 08:03:36 +0100

    Add files via upload

---
+++ StringOperations/StringStrip.java
@@ -0,0 +1,6 @@
+public class StringStrip {
+    public static void main(String[] args) {
+        String s = " This is some text with leading and trailing spaces ";
+        System.out.println(s.strip());
+    }
+}
