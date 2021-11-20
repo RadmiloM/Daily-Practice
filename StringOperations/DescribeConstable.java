42e6020b69046c56b37d369031437d4ce13a6470
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-20 18:34:55 +0100

    Add files via upload

---
+++ StringOperations/DescribeConstable.java
@@ -0,0 +1,6 @@
+public class DescribeConstable {
+    public static void main(String[] args) {
+        String s = "This is the way that works";
+        System.out.println(s.describeConstable());
+    }
+}
