7280db81b2dd5633ae5e02f02cab78dd4bd38b06
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-21 11:10:50 +0100

    Add files via upload

---
+++ StringOperations/StripTrailing.java
@@ -0,0 +1,6 @@
+public class StripTrailing {
+    public static void main(String[] args) {
+        String s = "Android developement is good place ";
+        System.out.println(s.stripTrailing());
+    }
+}
