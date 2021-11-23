e74524b579a7a0417e5ebe7ad75492866cc724c6
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-23 19:48:52 +0100

    Add files via upload

---
+++ StringOperations/StringFormated.java
@@ -0,0 +1,6 @@
+public class StringFormated {
+    public static void main(String[] args) {
+        String s = "This is string which require format";
+        System.out.println(s.formatted());
+    }
+}
