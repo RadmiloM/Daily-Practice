cdfa985f4a1ee511b82ebefc06470c009cb07ac7
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-11-24 10:53:02 +0100

    Add files via upload

---
+++ StringOperations/StringContent.java
@@ -0,0 +1,6 @@
+public class StringContent {
+    public static void main(String[] args) {
+        String str = "This is some text with content";
+        System.out.println(str.contentEquals("some"));
+    }
+}
